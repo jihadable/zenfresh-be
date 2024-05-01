5c69a2f735c06cb05e99acffb0d93eb95421b672
jihadable <umarjihad141004@gmail.com>
2024-05-01 17:23:41 +0700

    create deleteSingleLaundry and deleteMultipleLaundries method, refactor storeLaundry method

--- controllers/laundryController.js
+++ controllers/laundryController.js
@@ -15,11 +15,13 @@ const getSingleLaundry = async (req, res) => {
 const storeLaundry = async (req, res) => {
     try {
         const { user_id } = req.body
-        const laundry = await (await Laundry.create({ ...req.body, user: user_id })).populate("user")
+        const laundry = await Laundry.create({ ...req.body, user: user_id })
+
+        await laundry.populate("user")
 
         return res.status(201).json({
             status: 201,
-            message: "Laundry created",
+            message: "Laundry created successfully",
             laundry: laundry.response()
         })
     } catch (error){
@@ -27,9 +29,50 @@ const storeLaundry = async (req, res) => {
     }
 }
 
-// delete a laundry
-const deleteLaundry = async (req, res) => {
+// delete single laundry
+const deleteSingleLaundry = async (req, res) => {
+    try {
+        const { id } = req.params
 
+        const deletedLaundry = await Laundry.findByIdAndDelete(id)
+
+        if (!deletedLaundry){
+            return res.status(404).json({
+                status: 404,
+                message: "No laundry found with the provided ID"
+            })
+        }
+
+        return res.status(200).json({
+            status: 200,
+            message: "Laundry deleted successfully"
+        })
+    } catch (error){
+        errorResponse(error, res)
+    }
+}
+
+// delete multiple laundries
+const deleteMultipleLaundry = async (req, res) => {
+    try {
+        const { ids } = req.body
+
+        const deletedLaundry = await Laundry.deleteMany({ _id: { $in: ids }})
+
+        if (deletedLaundry.deletedCount === 0){
+            return res.status(404).json({
+                status: 404,
+                message: "No laundries found with the provided IDs"
+            })
+        }
+
+        return res.status(200).json({
+            status: 200,
+            message: "Laundries deleted successfully"
+        })
+    } catch (error){
+        errorResponse(error, res)
+    } 
 }
 
 // update a laundry
@@ -37,16 +80,23 @@ const updateLaundry = async (req, res) => {
     try {
         const { id } = req.params
     
-        const laundry = await Laundry.findByIdAndUpdate(id, { ...req.body }, { new: true }).populate("user")
+        const updatedLaundry = await Laundry.findByIdAndUpdate(id, { ...req.body }, { new: true }).populate("user")
+
+        if (!updatedLaundry){
+            return res.status(404).json({
+                status: 404,
+                message: "No laundry found with the provided ID"
+            })
+        }
 
         return res.status(200).json({
             status: 200,
-            message: "Laundry updated",
-            laundry: laundry.response()
+            message: "Laundry updated successfully",
+            laundry: updatedLaundry.response()
         })
     } catch (error){
         errorResponse(error, res)
     }
 }
 
-module.exports = { getAllLaundries, getSingleLaundry, storeLaundry, deleteLaundry, updateLaundry }
+module.exports = { getAllLaundries, getSingleLaundry, storeLaundry, deleteSingleLaundry, deleteMultipleLaundry, updateLaundry }
--- routes/laundryRoute.js
+++ routes/laundryRoute.js
@@ -1,5 +1,5 @@
 const { Router } = require("express")
-const { getAllLaundries, getSingleLaundry, storeLaundry, deleteLaundry, updateLaundry } = require("../controllers/laundryController")
+const { getAllLaundries, getSingleLaundry, storeLaundry, deleteSingleLaundry, updateLaundry, deleteMultipleLaundry } = require("../controllers/laundryController")
 const { extractUserId } = require("../middlewares/authMiddleware")
 const idValidation = require("../middlewares/idValidationMiddleware")
 
@@ -9,13 +9,16 @@ const laundryRouter = Router()
 laundryRouter.get("/", getAllLaundries)
 
 // get single laundry
-laundryRouter.get("/:id", getSingleLaundry)
+laundryRouter.get("/:id", extractUserId, idValidation, getSingleLaundry)
 
 // post a laundry
 laundryRouter.post("/", extractUserId, storeLaundry)
 
 // delete a laundry
-laundryRouter.delete("/:id", deleteLaundry)
+laundryRouter.delete("/:id", extractUserId, idValidation, deleteSingleLaundry)
+
+// delete multiple laundries
+laundryRouter.delete("/", extractUserId, deleteMultipleLaundry)
 
 // update a laundry
 laundryRouter.patch("/:id", extractUserId, idValidation, updateLaundry)
