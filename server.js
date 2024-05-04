cab1cadee3a9ac9389227b5b54a988be1bab05f6
jihadable <umarjihad141004@gmail.com>
2024-05-04 22:36:47 +0700

    create dist folder

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const cors = require("cors")
+const userRoute = require("./routes/userRoute")
+const laundryRouter = require("./routes/laundryRoute")
 
 require("dotenv").config()
 
 const app = express()
 const port = 8000
-const mongoose = require("mongoose")
-
-const userRoute = require("./routes/userRoute")
-const laundryRouter = require("./routes/laundryRoute")
 
 app.use(cors({ origin: process.env.FRONTEND_ENDPOINT }), express.json())
+
+app.get("/", (req, res) => {
+    res.json({
+        "status": "ok"
+    })
+})
 
 // user route
 app.use("/api/users", userRoute)
