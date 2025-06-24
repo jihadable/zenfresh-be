22e3fd0d158b6ee0e05ac39a5f1f802595562dac
jihadable <umarjihad141004@gmail.com>
2025-06-24 08:43:45 +0700

    use cors

--- test/graphql/testApp.js
+++ test/graphql/testApp.js
@@ -3,8 +3,11 @@ const express = require("express")
 const { createHandler } = require('graphql-http/lib/use/express');
 const schema = require("../../src/graphql");
 const connectDB = require("../../src/database/db");
+const cors = require("cors")
 
 const app = express()
+app.use(express.json(), cors())
+
 connectDB()
 
 app.use("/graphql", (req, res) => {
