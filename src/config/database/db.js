89e307f9a183af748499ff05aa3f927ed0b36cd0
jihadable <umarjihad141004@gmail.com>
2026-01-16 07:31:52 +0700

    clear cache

--- src/config/database/db.js
+++ src/config/database/db.js
@@ -3,7 +3,15 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+        const phase = process.env.APP_PHASE || "test"
+        let mongoURI
+        if (phase == "test"){
+            mongoURI = process.env.MONGO_URI_TEST
+        } else if (phase == "dev"){
+            mongoURI = process.env.MONGO_URI_DEV
+        }
+
+        await mongoose.connect(mongoURI)
     } catch (error) {
         console.error(error.message)
         process.exit(1)
